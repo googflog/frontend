e4d2e4b61db9bd9cc4c9f5dc879b3d73074f9bc1
t.tsujimoto <t.tsujimoto@futurek.com>
2018-12-21 13:20:26 +0900

    デモページ整理

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,38 +1,46 @@
+//ライブラリ埋め込み
 import $ from "jquery";
-import {TweenMax, Power2, TimelineLite} from "gsap/TweenMax";
+import {
+  TweenMax,
+  Power2,
+  TimelineLite
+} from "gsap/TweenMax";
+
 // sub.jsファイルを読み込む
-import {hello} from "./_sub";
+import {
+  hello
+} from "./_sub";
+
+//その他埋め込み
 import YTPlayer from "./module/_YTPlayer";
 import ShareSNS from "./module/_ShareSNS";
 
 // sub.jsに定義されたJavaScriptを実行する
 hello();
 
-var abc =()=>{
-  console.log("ABC2");
-}
-
-abc();
-
+// Youtube 埋め込み
 let ytp;
 YTPlayer.loadAPI(function() {
-  ytp = new YTPlayer("youtube", "3QJDC-kRwZU", 600, 0, {autoplay:0,rel:0,showinfo:0});
+  ytp = new YTPlayer("youtube", "3QJDC-kRwZU", 600, 0, {
+    autoplay: 0,
+    rel: 0,
+    showinfo: 0
+  });
 })
 
-let a="A2";
-console.log(a);
-let aaa = (ee2) =>{
-  console.log(ee2);
-}
-aaa("C")
-$(function(){
 
+$(function() {
+
+  // Youtube再生
   $(".playbtn").on("click", function(e) {
     e.preventDefault();
     ytp.play();
   })
 
+  // SNSボタン
   var shareSNS = new ShareSNS();
-  var shareSNS = new ShareSNS({twitter:'.js-tw'});
+  var shareSNS = new ShareSNS({
+    twitter: '.js-tw'
+  });
 
 })
